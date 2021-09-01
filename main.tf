157159d28d34689666886133f7b391a1080c8f4a
kharadegit <70315999+kharadegit@users.noreply.github.com>
2021-09-02 00:20:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ resource "aws_launch_configuration" "app_server" {
   key_name = var.key-name
   user_data = data.template_file.userdata.rendered
 # user = data.template_file.testdata.rendered
-  tags = {
-    Name = var.instance-name
-  }
+  #tags = {
+   # Name = var.instance-name
+  #}
 }
 #resource "aws_security_group" "sg" {
 #  tags = {
