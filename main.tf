e40196cea7448b23b42179d887f78cd03d57efb9
kharadegit <70315999+kharadegit@users.noreply.github.com>
2021-09-02 00:43:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
   secret_key  = "/J86ssw3ep1NkKNT7erV74gu3RDeu81UoNyYzSwm"
 }
 
-resource "aws_launch_configuration" "app_server" {
-  image_id           = var.ami
+resource "aws_instance" "app_server" {
+  ami           = var.ami
   instance_type = var.instance-type
   key_name = var.key-name
   user_data = data.template_file.userdata.rendered
