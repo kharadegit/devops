b5162a7a6e42e122265c5a204fa15aaa7954b831
kharadegit <70315999+kharadegit@users.noreply.github.com>
2021-09-02 10:16:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
 provider "aws" {
   profile = "default"
   region  = "ap-south-1"
-  access_key = "AKIAWESF7XJLJEWU6XUT"
-  secret_key  = "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"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_launch_configuration" "app_server" {
   ami           = var.ami
   instance_type = var.instance-type
   key_name = var.key-name
   user_data = data.template_file.userdata.rendered
   #user = data.template_file.testdata.rendered
-  tags = {
-    Name = var.instance-name
-  }
+  #tags = {
+   # Name = var.instance-name
+  #}
 }
 #resource "aws_security_group" "sg" {
 #  tags = {
