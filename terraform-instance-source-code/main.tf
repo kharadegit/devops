4082f21690589a654c8f65951be4e38f62451058
root <root@ip-172-31-4-3.ap-south-1.compute.internal>
2021-09-02 06:54:19 +0000

    added access key

--- terraform-instance-source-code/main.tf
+++ terraform-instance-source-code/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "${var.region}"
+  access_key = "AKIAWESF7XJLJEWU6XUT"
+  secret_key  = "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"
 }
+
 
 resource "aws_instance" "web" {
   ami           = "${var.ami_id}"
